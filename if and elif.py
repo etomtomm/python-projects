98aa49166b358fc570a46a6fa4b0a3595b09cf03
etomtomm <EtomtomM@Gmail.com>
2014-07-24 14:31:20 -0600

    If, Elif, and While True

---
+++ if and elif.py
@@ -0,0 +1,17 @@
+while True:
+
+    name = input("What is your name? ")
+
+    if name == "Sven":
+        print("Ohay. . . Sven")
+        break
+    
+    elif name == "Dereck":
+        print("I hate Dereck!")
+        break
+
+    
+    
+    else:
+     print("Hello " + name)
+
